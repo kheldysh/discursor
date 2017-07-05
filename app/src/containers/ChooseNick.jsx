5802db8b56ce45349da153443fea3cc27ed996e2
Jesse Lankila <jesse.lankila@relex.fi>
2017-07-06 02:58:48 +0300

    Don't show nick selection when nick is already chosen

--- app/src/containers/ChooseNick.jsx
+++ app/src/containers/ChooseNick.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { setNick } from '../actions'
 
-let ChooseNick = ({ dispatch }) => {
+let ChooseNick = ({ nick, dispatch }) => {
   let input
-
+  if (!!nick) {
+    return (
+      <div>
+        <h2>Hello, {nick}!</h2>
+      </div>
+    )
+  }
   return (
     <div>
       <form
@@ -30,6 +36,14 @@ let ChooseNick = ({ dispatch }) => {
   )
 }
 
-ChooseNick = connect()(ChooseNick)
+const mapStateToProps = state => {
+  return {
+    nick: state.get('nick')
+  }
+}
+
+ChooseNick = connect(
+  mapStateToProps
+)(ChooseNick)
 
 export default ChooseNick
