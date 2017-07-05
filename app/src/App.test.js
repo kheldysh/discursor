545b3466306289a26ef273cb57ebf9fe8e32f4f7
Jesse Lankila <jesse.lankila@relex.fi>
2017-07-05 15:16:29 +0300

    Restructuring: move react frontend project under /app, server side under /lib

--- app/src/App.test.js
+++ app/src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Provider from 'react-redux'
 import { createStore } from 'redux'
 import App from './App';
-import rootReducer from './reducers'
+import rootReducer from './rootReducer'
 
 // Using Provider seems to cause some issues in testing, skip testing
 // rendering of whole App for now
--- app/src/index.js
+++ app/src/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-import './app/index.css';
-import App from './app/App';
-import rootReducer from './app/rootReducer'
-import registerServiceWorker from './app/registerServiceWorker';
+import './index.css';
+import App from './App';
+import rootReducer from './rootReducer'
+import registerServiceWorker from './registerServiceWorker';
 
 let store = createStore(rootReducer)
 
