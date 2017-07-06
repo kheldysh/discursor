b79362a07eaa527f27f2e28b8e6f4f5ebf37af30
Jesse Lankila <jesse.lankila@relex.fi>
2017-07-06 06:15:32 +0300

    Fix App import pointing to old place

--- app/src/index.js
+++ app/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import './index.css';
-import App from './App';
+import App from './components/App';
 import Immutable from 'immutable'
 import rootReducer from './rootReducer'
 import registerServiceWorker from './registerServiceWorker';
