0deac19008b3917d549c74cafb6471c7c6779fe9
Jesse Lankila <jesse.lankila@relex.fi>
2017-07-06 04:25:58 +0300

    Move App to components

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import ChooseNick from './containers/ChooseNick'
-import Chat from './components/Chat'
-import './App.css';
+import ChooseNick from '../containers/ChooseNick'
+import Chat from './Chat'
+import '../App.css';
 
 class App extends Component {
   render() {
